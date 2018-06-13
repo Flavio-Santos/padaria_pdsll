648551669336b0bcbe451721bc02b293f3b4b0e8
Flavio Santos <flavio.santos.pro@gmail.com>
2018-06-13 02:19:55 -0300

    criacao primeiro dominio

--- src/main/java/br/com/padaria/resources/ProdutoResource.java
+++ src/main/java/br/com/padaria/resources/ProdutoResource.java
@@ -4,12 +4,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.padaria.domain.Produto;
+
 @RestController
 @RequestMapping(value="/produtos")
 public class ProdutoResource {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar(){
-		return "até que enfim";
+	public Produto listar(){
+		Produto prod = new Produto(1, "coxinha", 3.5, "feita de frango");
+		return prod;
 	}
 }
